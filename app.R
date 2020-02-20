9dd5a42b67debfa2f8258ef9fbd1373467056618
EM <em@EMs-MacBook-Pro.local>
2020-02-19 16:41:57 -0800

    beta3.2

--- app.R
+++ app.R
@@ -1348,8 +1348,13 @@ observeEvent(input$nextSDA_br2, {
   
   SDAorder <- 1:as.numeric(envv$SDAres$command_arguments$num_comps)
   
-  if(which(SDAorder==envv$QC_compIter) < length(SDAorder)){
-    envv$QC_compIter = SDAorder[which(SDAorder==envv$QC_compIter) + 1]
+  
+  
+  if(envv$QC_compIter + 1 %in% SDAorder){
+    # envv$QC_compIter = SDAorder[which(SDAorder==envv$QC_compIter) + 1]
+    # updateTextInput(session, "SDAVn", value = envv$QC_compIter)
+    updateTextInput(session, "SDAVn", value = SDAorder[which(SDAorder==envv$QC_compIter) + 1])
+    
   }
   
   
@@ -1365,38 +1370,63 @@ observeEvent(input$prevSDA_br2, {
   SDAorder <- 1:as.numeric(envv$SDAres$command_arguments$num_comps)
 
   
-  if(which(SDAorder==envv$QC_compIter) < length(SDAorder)){
-    envv$QC_compIter = SDAorder[which(SDAorder==envv$QC_compIter) - 1]
+  if(envv$QC_compIter - 1 %in% SDAorder){
+    #envv$QC_compIter = SDAorder[which(SDAorder==envv$QC_compIter) - 1]
+    # updateTextInput(session, "SDAVn", value = envv$QC_compIter)
+    updateTextInput(session, "SDAVn", value = SDAorder[which(SDAorder==envv$QC_compIter) - 1])
   }
   
   
+  
+  
 })
+
 
 observeEvent(input$SDAVn, {
   
   
   
-  # SDAorder <- 1:as.numeric(envv$SDAres$command_arguments$num_comps)
-  
-  if(!is.null(input$SDAVn)) {
-    if(!is.null(envv$QC_compIter)){
-      
-      choice <-  1:as.numeric(envv$SDAres$command_arguments$num_comps) #paste0("SDA", 1:as.numeric(envv$SDAres$command_arguments$num_comps)) # envv$QC_components
-      SDAorder <- setdiff(choice, envv$Remove_comps)
-      
-      input$SDAVn <- as.character(envv$QC_compIter)
-      
-      if(as.numeric(input$SDAVn) %in% SDAorder) {
-        
-        envv$QC_compIter = as.numeric(input$SDAVn)
-        
-      } else {
-        
-      }
-      
-    }}
+  if(!is.null(envv$QC_compIter)){
     
-
+    SDAorder <- 1:as.numeric(envv$SDAres$command_arguments$num_comps)
+    
+    if(is.null(input$SDAVn) | (!(as.numeric(input$SDAVn) %in% SDAorder))) {
+      
+      
+      print("Its null")
+      updateTextInput(session, "SDAVn", value = envv$QC_compIter)
+    } else {
+      envv$QC_compIter = as.numeric(input$SDAVn)
+    }
+  }
+  
+  
+  
+  # # SDAorder <- 1:as.numeric(envv$SDAres$command_arguments$num_comps)
+  # if(!is.null(envv$QC_compIter)){
+  #   
+  #   if(!is.null(input$SDAVn)) {
+  #     
+  #     
+  #   } else {
+  #     # input$SDAVn <- envv$QC_compIter
+  #   }
+  # } else {
+  #     
+  #   }
+    
+  # choice <-  1:as.numeric(envv$SDAres$command_arguments$num_comps) #paste0("SDA", 1:as.numeric(envv$SDAres$command_arguments$num_comps)) # envv$QC_components
+  # SDAorder <- setdiff(choice, envv$Remove_comps)
+  # 
+  # input$SDAVn <- as.character(envv$QC_compIter)
+  # 
+  # if(as.numeric(input$SDAVn) %in% SDAorder) {
+  # 
+  #   envv$QC_compIter = as.numeric(input$SDAVn)
+  # 
+  # } else {
+    
+  
   
   
   
